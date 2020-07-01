1878acea66b2393494d38cbc898c82ee66a938a0
Tia Hwang <tiajshwang@gmail.com>
2020-07-01 19:47:56 +0900

    add Infinite Scroll in TV page

--- src/Routes/List/ListContainer.js
+++ src/Routes/List/ListContainer.js
@@ -7,6 +7,9 @@ export default class extends React.Component {
         nowPlayingList: [],
         upcomingList: [],
         popularMovieList: [],
+        topRatedList: [],
+        airingList: [],
+        popularTvList: [],
         pageNumber: 1,
         hasMore: true,
     };
@@ -15,55 +18,85 @@ export default class extends React.Component {
         this.fetchMovieData();
     }
 
+    componentDidUpdate() {
+        window.scrollTo(0, 0);
+    }
+
     fetchMovieData = async () => {
         const {
             location: { pathname },
         } = this.props;
-        if (pathname.includes("now")) {
-            await axios
-                .get(
-                    `https://api.themoviedb.org/3/movie/now_playing?api_key=1f76b07ff6c77afe1ce9a752c92b8290&language=en-US&page=${this.state.pageNumber}`
-                )
-                .then((res) =>
-                    this.setState({
-                        nowPlayingList: [...this.state.nowPlayingList, ...res.data.results],
-                        pageNumber: this.state.pageNumber + 1,
-                    })
-                );
-        } else if (pathname.includes("upcoming")) {
-            await axios
-                .get(
-                    `https://api.themoviedb.org/3/movie/upcoming?api_key=1f76b07ff6c77afe1ce9a752c92b8290&language=en-US&page=${this.state.pageNumber}`
-                )
-                .then((res) =>
-                    this.setState({
-                        upcomingList: [...this.state.upcomingList, ...res.data.results],
-                        pageNumber: this.state.pageNumber + 1,
-                    })
-                );
-        } else if (pathname.includes("popular")) {
-            await axios
-                .get(
-                    `https://api.themoviedb.org/3/movie/popular?api_key=1f76b07ff6c77afe1ce9a752c92b8290&language=en-US&page=${this.state.pageNumber}`
-                )
-                .then((res) =>
-                    this.setState({
-                        popularMovieList: [...this.state.popularMovieList, ...res.data.results],
-                        pageNumber: this.state.pageNumber + 1,
-                    })
-                );
+        const { pageNumber, nowPlayingList, upcomingList, popularMovieList } = this.state;
+        const api = "https://api.themoviedb.org/3/movie";
+        const params = "?api_key=1f76b07ff6c77afe1ce9a752c92b8290&language=en-US";
+        if (pathname.includes("movie") && pathname.includes("now")) {
+            await axios.get(`${api}/now_playing${params}&page=${pageNumber}`).then((res) =>
+                this.setState({
+                    nowPlayingList: [...nowPlayingList, ...res.data.results],
+                    pageNumber: pageNumber + 1,
+                })
+            );
+        } else if (pathname.includes("movie") && pathname.includes("upcoming")) {
+            await axios.get(`${api}/upcoming${params}&page=${pageNumber}`).then((res) =>
+                this.setState({
+                    upcomingList: [...upcomingList, ...res.data.results],
+                    pageNumber: pageNumber + 1,
+                })
+            );
+        } else if (pathname.includes("movie") && pathname.includes("popular")) {
+            await axios.get(`${api}/popular${params}&page=${pageNumber}`).then((res) =>
+                this.setState({
+                    popularMovieList: [...popularMovieList, ...res.data.results],
+                    pageNumber: pageNumber + 1,
+                })
+            );
+        }
+    };
+
+    fetchTvData = async () => {
+        const {
+            location: { pathname },
+        } = this.props;
+        const { topRatedList, airingList, popularTvList, pageNumber } = this.state;
+        const api = "https://api.themoviedb.org/3/tv";
+        const params = "?api_key=1f76b07ff6c77afe1ce9a752c92b8290&language=en-US";
+        if (pathname.includes("tv") && pathname.includes("top")) {
+            await axios.get(`${api}/top_rated${params}&page=${pageNumber}`).then((res) =>
+                this.setState({
+                    topRatedList: [...topRatedList, ...res.data.results],
+                    pageNumber: pageNumber + 1,
+                })
+            );
+        } else if (pathname.includes("tv") && pathname.includes("airing")) {
+            await axios.get(`${api}/airing_today${params}&page=${pageNumber}`).then((res) =>
+                this.setState({
+                    airingList: [...airingList, ...res.data.results],
+                    pageNumber: pageNumber + 1,
+                })
+            );
+        } else if (pathname.includes("tv") && pathname.includes("popular")) {
+            await axios.get(`${api}/popular${params}&page=${pageNumber}`).then((res) =>
+                this.setState({
+                    popularTvList: [...popularTvList, ...res.data.results],
+                    pageNumber: pageNumber + 1,
+                })
+            );
         }
     };
 
     render() {
-        const { nowPlayingList, upcomingList, popularMovieList, hasMore } = this.state;
+        const { nowPlayingList, upcomingList, popularMovieList, airingList, popularTvList, topRatedList, hasMore } = this.state;
         return (
             <ListPresenter
                 nowPlayingList={nowPlayingList}
                 upcomingList={upcomingList}
                 popularMovieList={popularMovieList}
+                topRatedList={topRatedList}
+                airingList={airingList}
+                popularTvList={popularTvList}
                 hasMore={hasMore}
                 fetchMovieData={this.fetchMovieData}
+                fetchTvData={this.fetchTvData}
             />
         );
     }
--- src/Routes/List/ListPresenter.js
+++ src/Routes/List/ListPresenter.js
@@ -26,85 +26,164 @@ const PosterContainer = styled.div`
     justify-content: space-around;
 `;
 
-const ListPresenter = withRouter(({ location: { pathname }, nowPlayingList, upcomingList, popularMovieList, hasMore, fetchMovieData }) => (
-    <>
-        <Container current={pathname.includes("now")}>
-            {nowPlayingList && (
-                <InfiniteScroll
-                    dataLength={nowPlayingList.length} //This is important field to render the next data
-                    next={fetchMovieData}
-                    hasMore={hasMore}
-                    loader={<Loader />}
-                >
-                    <Title>Now Playing</Title>
-                    <PosterContainer>
-                        {nowPlayingList.length > 0 &&
-                            nowPlayingList.map((movie) => (
-                                <Poster
-                                    key={movie.id}
-                                    id={movie.id}
-                                    rating={movie.vote_average}
-                                    title={movie.title}
-                                    imageUrl={movie.poster_path}
-                                    year={movie.release_date && movie.release_date.substring(0, 4)}
-                                    isMovie={true}
-                                />
-                            ))}
-                    </PosterContainer>
-                </InfiniteScroll>
-            )}
-        </Container>
-        <Container current={pathname.includes("upcoming")}>
-            {upcomingList && (
-                <InfiniteScroll dataLength={upcomingList.length} next={fetchMovieData} hasMore={hasMore} loader={<Loader />}>
-                    <Title>Upcoming Movies</Title>
-                    <PosterContainer>
-                        {upcomingList.length > 0 &&
-                            upcomingList.map((movie) => (
-                                <Poster
-                                    key={movie.id}
-                                    id={movie.id}
-                                    rating={movie.vote_average}
-                                    title={movie.title}
-                                    imageUrl={movie.poster_path}
-                                    year={movie.release_date && movie.release_date.substring(0, 4)}
-                                    isMovie={true}
-                                />
-                            ))}
-                    </PosterContainer>
-                </InfiniteScroll>
-            )}
-        </Container>
-        <Container current={pathname.includes("popular")}>
-            {popularMovieList && (
-                <InfiniteScroll dataLength={popularMovieList.length} next={fetchMovieData} hasMore={hasMore} loader={<Loader />}>
-                    <Title>Popular Movies</Title>
-                    <PosterContainer>
-                        {popularMovieList.length > 0 &&
-                            popularMovieList.map((movie) => (
-                                <Poster
-                                    key={movie.id}
-                                    id={movie.id}
-                                    rating={movie.vote_average}
-                                    title={movie.title}
-                                    imageUrl={movie.poster_path}
-                                    year={movie.release_date && movie.release_date.substring(0, 4)}
-                                    isMovie={true}
-                                />
-                            ))}
-                    </PosterContainer>
-                </InfiniteScroll>
-            )}
-        </Container>
-    </>
-));
+const ListPresenter = withRouter(
+    ({
+        location: { pathname },
+        nowPlayingList,
+        upcomingList,
+        popularMovieList,
+        topRatedList,
+        airingList,
+        popularTvList,
+        hasMore,
+        fetchMovieData,
+        fetchTvData,
+    }) =>
+        pathname.includes("movie") ? (
+            <>
+                <Container current={pathname.includes("now")}>
+                    {nowPlayingList && (
+                        <InfiniteScroll dataLength={nowPlayingList.length} next={fetchMovieData} hasMore={hasMore} loader={<Loader />}>
+                            <Title>Now Playing</Title>
+                            <PosterContainer>
+                                {nowPlayingList.length > 0 &&
+                                    nowPlayingList.map((movie) => (
+                                        <Poster
+                                            key={movie.id}
+                                            id={movie.id}
+                                            rating={movie.vote_average}
+                                            title={movie.title}
+                                            imageUrl={movie.poster_path}
+                                            year={movie.release_date && movie.release_date.substring(0, 4)}
+                                            isMovie={true}
+                                        />
+                                    ))}
+                            </PosterContainer>
+                        </InfiniteScroll>
+                    )}
+                </Container>
+                <Container current={pathname.includes("upcoming")}>
+                    {upcomingList && (
+                        <InfiniteScroll dataLength={upcomingList.length} next={fetchMovieData} hasMore={hasMore} loader={<Loader />}>
+                            <Title>Upcoming Movies</Title>
+                            <PosterContainer>
+                                {upcomingList.length > 0 &&
+                                    upcomingList.map((movie) => (
+                                        <Poster
+                                            key={movie.id}
+                                            id={movie.id}
+                                            rating={movie.vote_average}
+                                            title={movie.title}
+                                            imageUrl={movie.poster_path}
+                                            year={movie.release_date && movie.release_date.substring(0, 4)}
+                                            isMovie={true}
+                                        />
+                                    ))}
+                            </PosterContainer>
+                        </InfiniteScroll>
+                    )}
+                </Container>
+                <Container current={pathname.includes("popular")}>
+                    {popularMovieList && (
+                        <InfiniteScroll dataLength={popularMovieList.length} next={fetchMovieData} hasMore={hasMore} loader={<Loader />}>
+                            <Title>Popular Movies</Title>
+                            <PosterContainer>
+                                {popularMovieList.length > 0 &&
+                                    popularMovieList.map((movie) => (
+                                        <Poster
+                                            key={movie.id}
+                                            id={movie.id}
+                                            rating={movie.vote_average}
+                                            title={movie.title}
+                                            imageUrl={movie.poster_path}
+                                            year={movie.release_date && movie.release_date.substring(0, 4)}
+                                            isMovie={true}
+                                        />
+                                    ))}
+                            </PosterContainer>
+                        </InfiniteScroll>
+                    )}
+                </Container>
+            </>
+        ) : (
+            <>
+                <Container current={pathname.includes("top")}>
+                    {topRatedList && (
+                        <InfiniteScroll dataLength={topRatedList.length} next={fetchTvData} hasMore={hasMore} loader={<Loader />}>
+                            <Title>Top Rated Shows</Title>
+                            <PosterContainer>
+                                {topRatedList.length > 0 &&
+                                    topRatedList.map((tv) => (
+                                        <Poster
+                                            key={tv.id}
+                                            id={tv.id}
+                                            rating={tv.vote_average}
+                                            title={tv.name}
+                                            imageUrl={tv.poster_path}
+                                            year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+                                            isMovie={false}
+                                        />
+                                    ))}
+                            </PosterContainer>
+                        </InfiniteScroll>
+                    )}
+                </Container>
+                <Container current={pathname.includes("airing")}>
+                    {airingList && (
+                        <InfiniteScroll dataLength={airingList.length} next={fetchTvData} hasMore={hasMore} loader={<Loader />}>
+                            <Title>Airing Today</Title>
+                            <PosterContainer>
+                                {airingList.length > 0 &&
+                                    airingList.map((tv) => (
+                                        <Poster
+                                            key={tv.id}
+                                            id={tv.id}
+                                            rating={tv.vote_average}
+                                            title={tv.name}
+                                            imageUrl={tv.poster_path}
+                                            year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+                                            isMovie={false}
+                                        />
+                                    ))}
+                            </PosterContainer>
+                        </InfiniteScroll>
+                    )}
+                </Container>
+                <Container current={pathname.includes("popular")}>
+                    {popularTvList && (
+                        <InfiniteScroll dataLength={popularTvList.length} next={fetchTvData} hasMore={hasMore} loader={<Loader />}>
+                            <Title>Popular Shows</Title>
+                            <PosterContainer>
+                                {popularTvList.length > 0 &&
+                                    popularTvList.map((tv) => (
+                                        <Poster
+                                            key={tv.id}
+                                            id={tv.id}
+                                            rating={tv.vote_average}
+                                            title={tv.name}
+                                            imageUrl={tv.poster_path}
+                                            year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+                                            isMovie={false}
+                                        />
+                                    ))}
+                            </PosterContainer>
+                        </InfiniteScroll>
+                    )}
+                </Container>
+            </>
+        )
+);
 
 ListPresenter.propTypes = {
     nowPlayingList: PropTypes.array,
     upcomingList: PropTypes.array,
     popularMovieList: PropTypes.array,
+    topRatedList: PropTypes.array,
+    airingList: PropTypes.array,
+    popularTvList: PropTypes.array,
     hasMore: PropTypes.bool,
     fetchMovieData: PropTypes.func,
+    fetchTvData: PropTypes.func,
 };
 
 export default ListPresenter;
